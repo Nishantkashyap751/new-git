7057360559c127f37b418d7742d25bce9f4fbfbb
NishantKashyap <nishanttkashyapp7988@gmail.com>
2025-09-01 10:25:08 +0530

    question1

---
+++ question1.java
@@ -0,0 +1,17 @@
+//1. Write a Java program to declare and print all primitive data types:-
+public class question1 {
+    
+    public static void main(String ar[]){
+        int variable1=1;
+    float variable2=2.f;
+    double variable3=3.666;
+    char variable4= 'A';
+    boolean variable5= true ;
+    System.out.println("Integer =" +variable1);
+        System.out.println("float =" +variable2);
+            System.out.println("double =" +variable3);
+                System.out.println("char =" +variable4);
+                    System.out.println("boolean =" +variable5);
+
+    }
+}
